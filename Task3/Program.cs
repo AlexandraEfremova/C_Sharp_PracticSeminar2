72a25c9dbb92449c4b20fcf32ea10badf68979a4
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-05 20:29:37 +0300

    PracticSeminar2 : Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+// целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+System.Console.WriteLine("Введите число от 10 до 99");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int firstNum = userNumber / 10;
+int secondNum = userNumber % 10;
+
+if(userNumber >= 10 && userNumber <= 99)
+{
+    if(firstNum > secondNum)
+    {
+        System.Console.WriteLine($"В числе {userNumber} больше первая цифра: {firstNum}");
+    }
+    else
+    {
+        System.Console.WriteLine($"В числе {userNumber} больше вторая цифра: {secondNum}");
+    }
+}
+else
+{
+    System.Console.WriteLine($"Заданное Вами число: {userNumber} не подходит под условие.\n Прочтите условие. Попробуйте ещё раз");
+}
