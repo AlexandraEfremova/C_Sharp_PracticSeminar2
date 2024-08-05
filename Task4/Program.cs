4db7baf3991e57a47551b06dd68b22a5e3acc4b9
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-05 20:45:50 +0300

    PracticSeminar2 : Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите любое натуральное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+while (userNumber > 0)
+{
+    number = userNumber % 10;
+    System.Console.Write($"{number}, ");
+    userNumber = userNumber / 10;
+}
